54b3326378250a829edb0166fba5aff52a3e7bd9
Joe Stump <joe@joestump.net>
2008-04-04 03:52:26 +0000

    Nuked old branches/trunk/tags directory.

--- tags/0.0.8/tests/tests-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$path = ini_get('include_path');
-ini_set('include_path', realpath('../') . ':' . $path);
-
-// The Gearman servers to use for the various tests
-$servers = array('dev01:7003', 'dev01:7004');
-
-?>
