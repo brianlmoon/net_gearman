34d4c4ef8fe818662398ce337717f4554f1931a2
Joe Stump <joe@joestump.net>
2008-04-07 23:56:22 +0000

    Refs #3.  1. Added Net_Gearman_Manager class for managing Gearmand  2. Cleaned up Net_Gearman_Client for phpcs.

--- Net/Gearman/Client.php
+++ Net/Gearman/Client.php
@@ -11,14 +11,14 @@
  * a copy of the New BSD License and are unable to obtain it through the web, 
  * please send a note to license@php.net so we can mail you a copy immediately.
  *
- * @category    Net
- * @package     Net_Gearman
- * @author      Joe Stump <joe@joestump.net> 
- * @copyright   2007 Digg.com, Inc.
- * @license     http://www.opensource.org/licenses/bsd-license.php 
- * @version     CVS: $Id:$
- * @link        http://pear.php.net/package/Net_Gearman
- * @link        http://www.danga.com/gearman/
+ * @category  Net
+ * @package   Net_Gearman
+ * @author    Joe Stump <joe@joestump.net> 
+ * @copyright 2007-2008 Digg.com, Inc.
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/Net_Gearman
+ * @link      http://www.danga.com/gearman/
  */ 
 
 require_once 'Net/Gearman/Connection.php';
@@ -30,45 +30,45 @@ require_once 'Net/Gearman/Set.php';
  * This class is used by code submitting jobs to the Gearman server. It handles
  * taking tasks and sets of tasks and submitting them to the Gearman server.
  *
- * @category    Net
- * @package     Net_Gearman
- * @author      Joe Stump <joe@joestump.net> 
- * @link        http://www.danga.com/gearman/
+ * @category  Net
+ * @package   Net_Gearman
+ * @author    Joe Stump <joe@joestump.net> 
+ * @copyright 2007-2008 Digg.com, Inc.
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link      http://www.danga.com/gearman/
  */
 class Net_Gearman_Client
 {
     /**
      * Our randomly selected connection
      *
-     * @access      protected
-     * @var         resource    $conn       An open socket to Gearman
+     * @var resource $conn An open socket to Gearman
      */
     protected $conn = array();
 
     /**
      * A list of Gearman servers
      *
-     * @access      protected
-     * @var         array       $servers    A list of potential Gearman servers
+     * @var array $servers A list of potential Gearman servers
      */
     protected $servers = array();
 
     /**
      * The timeout for Gearman connections
      *
-     * @access      protected
-     * @var         integer     $timeout
+     * @var integer $timeout
      */
     protected $timeout = 1000;
 
     /**
      * Constructor
      *
-     * @access      public
-     * @param       array       $servers
-     * @param       integer     $timeout        Timeout in microseconds
-     * @return      void
-     * @see         Net_Gearman_Connection
+     * @param array   $servers An array of servers or a single server
+     * @param integer $timeout Timeout in microseconds
+     * 
+     * @return void
+     * @throws Net_Gearman_Exception
+     * @see Net_Gearman_Connection
      */
     public function __construct($servers, $timeout = 1000)
     {
@@ -95,8 +95,7 @@ class Net_Gearman_Client
     /**
      * Get a connection to a Gearman server
      *
-     * @access      public
-     * @return      resource    A connection to a Gearman server
+     * @return resource A connection to a Gearman server
      */
     protected function getConnection()
     {
@@ -106,10 +105,11 @@ class Net_Gearman_Client
     /**
      * Fire off a background task with the given arguments
      *
-     * @access      public
-     * @param       string      $func       Name of job to run
-     * @param       array       $args       First key should be args to send 
-     * @return      void
+     * @param string $func Name of job to run
+     * @param array  $args First key should be args to send 
+     *
+     * @return void
+     * @see Net_Gearman_Task, Net_Gearman_Set
      */
     public function __call($func, array $args = array())
     {
@@ -118,7 +118,7 @@ class Net_Gearman_Client
             $send = $args[0];
         }
 
-        $task = new Net_Gearman_Task($func, $send);
+        $task       = new Net_Gearman_Task($func, $send);
         $task->type = Net_Gearman_Task::JOB_BACKGROUND;
 
         $set = new Net_Gearman_Set();
@@ -130,8 +130,8 @@ class Net_Gearman_Client
     /**
      * Submit a task to Gearman
      *
-     * @access      protected
-     * @param       object      $task       Task to submit to Gearman
+     * @param object $task Task to submit to Gearman
+     * 
      * @return      void
      * @see         Net_Gearman_Task, Net_Gearman_Client::runSet()
      */
@@ -168,16 +168,17 @@ class Net_Gearman_Client
     /**
      * Run a set of tasks
      *
-     * @access      public
-     * @param       object      $set        A set of tasks to run
-     * @return      void
-     * @see         Net_Gearman_Set, Net_Gearman_Task
+     * @param object $set A set of tasks to run
+     * 
+     * @return void
+     * @see Net_Gearman_Set, Net_Gearman_Task
      */
     public function runSet(Net_Gearman_Set $set) 
     {
         $totalTasks = $set->tasksCount;
-        $taskKeys = array_keys($set->tasks);
-        $t = 0;
+        $taskKeys   = array_keys($set->tasks);
+        $t          = 0;
+
         while (!$set->finished()) {
             if ($t < $totalTasks) {
                 $k = $taskKeys[$t];
@@ -190,8 +191,9 @@ class Net_Gearman_Client
                 $t++;
             }
 
-            $write = $except = null;
-            $read = $this->conn;
+            $write  = null;
+            $except = null;
+            $read   = $this->conn;
             socket_select($read, $write, $except, 10);
             foreach ($read as $socket) {
                 $resp = Net_Gearman_Connection::read($socket);
@@ -205,12 +207,12 @@ class Net_Gearman_Client
     /**
      * Handle the response read in 
      *
-     * @access      protected
-     * @param       array       $resp       The raw array response
-     * @param       resource    $s          The socket 
-     * @param       object      $tasks      The tasks being ran
-     * @return      void
-     * @throws      Net_Gearman_Exception
+     * @param array    $resp  The raw array response
+     * @param resource $s     The socket 
+     * @param object   $tasks The tasks being ran
+     * 
+     * @return void
+     * @throws Net_Gearman_Exception
      */
     protected function handleResponse($resp, $s, Net_Gearman_Set $tasks) 
     {
@@ -234,7 +236,7 @@ class Net_Gearman_Client
             $task->fail();
             break;
         case 'job_created':
-            $task = array_shift(Net_Gearman_Connection::$waiting[$s]);
+            $task         = array_shift(Net_Gearman_Connection::$waiting[$s]);
             $task->handle = $resp['data']['handle'];
             if ($task->type == Net_Gearman_Task::JOB_BACKGROUND) {
                 $task->finished = true;
@@ -244,14 +246,15 @@ class Net_Gearman_Client
         case 'error':
             throw new Net_Gearman_Exception('An error occurred');
         default:
-            throw new Net_Gearman_Exception('Invalid function ' . $resp['function']); 
+            throw new Net_Gearman_Exception(
+                'Invalid function ' . $resp['function']
+            ); 
         }
     }
 
     /**
      * Disconnect from Gearman
      *
-     * @access      public
      * @return      void
      */
     public function disconnect()
@@ -268,7 +271,6 @@ class Net_Gearman_Client
     /**
      * Destructor
      *
-     * @access      public
      * @return      void
      */
     public function __destruct()
