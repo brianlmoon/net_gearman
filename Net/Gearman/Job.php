b31433b2d180d6a8c5a9a4fc6c4198e4f2fb0f35
Jason Pirkey <jason@tave.com>
2014-04-13 15:20:57 -0400

    Need to parens the concat.

--- Net/Gearman/Job.php
+++ Net/Gearman/Job.php
@@ -70,7 +70,7 @@ abstract class Net_Gearman_Job
         include_once $file;
         $class = NET_GEARMAN_JOB_CLASS_PREFIX . $job;
         if (!class_exists($class)) {
-            throw new Net_Gearman_Job_Exception('Invalid Job class: ' . empty($class) ? '<empty>' : $file);
+            throw new Net_Gearman_Job_Exception('Invalid Job class: ' . (empty($class) ? '<empty>' : $file));
         }
 
         $instance = new $class($conn, $handle, $initParams);
