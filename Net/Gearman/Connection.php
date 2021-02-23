f893d2cf633d1b18695d06c82f9390ad5bfa37e4
Jason <me@j03k64.com>
2021-02-22 20:42:37 -0500

    Breaking up the logic a bit more for readability

--- Net/Gearman/Connection.php
+++ Net/Gearman/Connection.php
@@ -476,16 +476,21 @@ class Net_Gearman_Connection
      */
     public function isConnected()
     {
-        // PHP 8+ returns Socket class instead of resource
+        if ($this->socket === null) {
+            // no socket established yet
+            return false;
+        }
+
+        // PHP 8+ returns Socket object instead of resource
         if ($this->socket instanceof Socket) {
             return true;
         }
-        
+
         // HHVM returns stream. PHP 5.x returns socket
-        $type = strtolower(get_resource_type($this->socket));
-        return (is_null($this->socket) !== true &&
-                is_resource($this->socket) === true &&
-                ($type == 'socket' || $type == "stream"));
+        $type = mb_strtolower(get_resource_type($this->socket));
+
+        return is_resource($this->socket) === true
+            && ($type == 'socket' || $type == 'stream');
     }
 
     /**
